dd51fad50d05d5c8d09b7d855ac833f133ace7b7
SimoPrG <simeon_petev@hotmail.com>
2015-08-03 13:53:52 +0300

    jQuery Overview Task 1

--- jQueryOverview/tasks/task-1.js
+++ jQueryOverview/tasks/task-1.js
@@ -1,8 +1,9 @@
+//@formatter:off
 /* globals $ */
 
 /* 
 
-Create a function that takes a selector and COUNT, then generates inside a UL with COUNT LIs:   
+Create a function that takes a selector and COUNT, then generates inside a UL with COUNT LIs:
   * The UL must have a class `items-list`
   * Each of the LIs must:
     * have a class `list-item`
@@ -16,13 +17,41 @@ Create a function that takes a selector and COUNT, then generates inside a UL wi
         * Valid COUNT values:
           * 1, 2, 3, '1', '4', '1123'
         * Invalid COUNT values:
-          * '123px' 'John', {}, [] 
+          * '123px' 'John', {}, []
 */
+//@formatter:on
 
 function solve() {
-  return function (selector, count) {
-   
-  };
-};
+    function validateIfString(string) {
+        if (typeof string !== 'string') {
+            throw new TypeError('selector must be a string');
+        }
+    }
+    function validateIfPositiveInteger(number) {
+        if (number != parseInt(number) || number < 1) {
+            throw new Error('count must be a positive integer');
+        }
+    }
+
+    return function (selector, count) {
+        validateIfString(selector);
+        validateIfPositiveInteger(count);
+        var $element = $(selector),
+            $ul,
+            $li,
+            i;
+
+        if ($element.length === 0) {
+            return;
+        }
+
+        $ul = $('<ul />').addClass('items-list');
+        for (i = 0; i < count; i += 1) {
+            $li = $('<li />').addClass('list-item').text('List item #' + i).appendTo($ul);
+        }
+
+        $element.append($ul);
+    };
+}
 
 module.exports = solve;
